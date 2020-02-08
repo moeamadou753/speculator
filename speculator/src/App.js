084fcf925db0cb6f537e9204755b336b37916acb
Md Aiman Sharif <sharif.aiman@gmail.com>
2020-02-08 17:17:13 -0500

    Search bar added

--- speculator/src/App.js
+++ speculator/src/App.js
@@ -3,12 +3,12 @@ import './scss/main.scss';
 import { Component } from 'react';
 import ReactBootstrap from 'react-bootstrap';
 import Card from "./components/base/Card";
+import SearchBar from './components/SearchBar.js';
 
 function App() {
   return (
     <div className="App">
-      <Card>
-      </Card>
+
     </div>
   );
 }
