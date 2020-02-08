aea8b071cba52b776ccd952c74eba9226f1777f5
Cedric Knapp <cedriccknapp@gmail.com>
2020-02-08 17:20:11 -0500

    Removed react-bootstrap

--- speculator/src/App.js
+++ speculator/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './scss/main.scss';
 import { Component } from 'react';
-import ReactBootstrap from 'react-bootstrap';
-import Card from "./components/base/Card";
 
 function App() {
   return (
     <div className="App">
-      <Card>
-      </Card>
+
     </div>
   );
 }
