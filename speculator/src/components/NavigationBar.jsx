175d4769deea170df64b0ca4f1217dca7e0bda70
Alex He <alexhe2001@gmail.com>
2020-02-08 20:05:37 -0500

    Border for nav. bar

--- speculator/src/components/NavigationBar.jsx
+++ speculator/src/components/NavigationBar.jsx
@@ -1,38 +1,39 @@
 import React from "react";
 import { BrowserRouter as Router, NavLink } from "react-router-dom";
 import "../scss/NavigationBar.scss";
+import Card from "react-bootstrap/Card";
 
 function NavigationBar() {
   return (
-    <div className="navbar__container">
-      <NavLink
-        className="navbar__text-inactive h3"
-        exact
-        to="/#about"
-        activeClassName="navbar__text-active h3"
-        isActive={isActive("#about")}
-      >
-        <div>about</div>
-      </NavLink>
-      <NavLink
-        className="navbar__text-inactive h3"
-        exact
-        strict
-        to="/#gettingstarted"
-        activeClassName="navbar__text-active h3"
-        isActive={isActive("#gettingstarted")}
-      >
-        <div>getting started</div>
-      </NavLink>
-      <NavLink
-        className="navbar__text-inactive h3"
-        exact
-        to="/securities"
-        activeClassName="navbar__text-active h3"
-      >
-        <div>speculator securities</div>
-      </NavLink>
-    </div>
+      <div className="navbar__container">
+        <NavLink
+          className="navbar__text-inactive h3"
+          exact
+          to="/#about"
+          activeClassName="navbar__text-active h3"
+          isActive={isActive("#about")}
+        >
+          <div>about</div>
+        </NavLink>
+        <NavLink
+          className="navbar__text-inactive h3"
+          exact
+          strict
+          to="/#gettingstarted"
+          activeClassName="navbar__text-active h3"
+          isActive={isActive("#gettingstarted")}
+        >
+          <div>getting started</div>
+        </NavLink>
+        <NavLink
+          className="navbar__text-inactive h3"
+          exact
+          to="/securities"
+          activeClassName="navbar__text-active h3"
+        >
+          <div>speculator securities</div>
+        </NavLink>
+      </div>
   );
 }
 
